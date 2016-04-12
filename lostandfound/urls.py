950934d15a0e65f43c09f987f330fba6b27b2ba7
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-04-12 15:37:41 -0700

    Add error templates

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import include, url
+from django.http import HttpResponseServerError
+from django.template import loader
+from django.views.decorators.csrf import requires_csrf_token
 
 from .items import views as items
 from .views import home
@@ -21,3 +24,22 @@ urlpatterns = [
 
     url(r'^cloak/', include('cloak.urls')),
 ]
+
+
+@requires_csrf_token
+def server_error(request, template_name='500.html'):
+    """Override default Django server_error view so context is passed.
+
+    Otherwise, static files won't be loaded and default context vars
+    won't be available (&c).
+
+    If loading or rendering the template causes an error, a bare 500
+    response will be returned.
+
+    """
+    try:
+        template = loader.get_template(template_name)
+        body, content_type = template.render(request=request), None
+    except Exception:
+        body, content_type = '<h1>Server Error (500)</h1>', 'text/html'
+    return HttpResponseServerError(body, content_type=content_type)
