4afff98fd7476d7b655dedb6fc64601b4e7df3ee
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-05-04 10:51:24 -0700

    Remove unused import in urls module

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.http import HttpResponseServerError
 from django.template import loader
 from django.views.decorators.csrf import requires_csrf_token
